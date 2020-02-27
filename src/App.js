ffd644bb548f9689d54b09b93186bb8566e67f72
Mauro de Souza <maurodesouza2017@hotmail.com>
2020-02-27 10:55:09 -0300

    📕  class 02 - hook useState

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <h1>Hmm ... Hellow ?</h1>;
+  const [tasks, setTasks] = useState([]);
+  const [newtask, setNewTask] = useState([]);
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setTasks([...tasks, newtask]);
+    setNewTask('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <ul>
+        {tasks.map(task => (
+          <li key={`${task}${Math.random()}`}>{task}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newtask}
+        onChange={e => setNewTask(e.target.value)}
+      />
+      <button type="submit"> Adicionar </button>
+    </form>
+  );
 }
 
 export default App;
